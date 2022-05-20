ab03081422885d0f7d556fa800c3a94ff8baad2b
Kaunain Ahmad <kaunain.ahmad2000@gmail.com>
2022-05-20 15:26:55 +0530

    Setup JAva Console

---
+++ java-coding/src/main/java/com/itechf/javacoding/Basic.java
@@ -0,0 +1,16 @@
+package com.itechf.javacoding;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * How to reverse a String in Java?
+ */
+
+ @Component
+public class Basic {
+
+    public void reverseString(String input) {
+        System.out.println(input);
+    }
+    
+}
---
+++ java-coding/src/main/java/com/itechf/javacoding/JavaCodingApplication.java
@@ -0,0 +1,33 @@
+package com.itechf.javacoding;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodingApplication implements CommandLineRunner {
+
+	@Autowired
+	private Basic basic;
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodingApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Started...");
+		System.out.println("---------------------------------");
+		System.out.println();
+		
+		basic.reverseString("Core Java");
+		
+		
+		System.out.println();
+		System.out.println("---------------------------------");
+		System.out.println("Completed");
+		
+	}
+
+}
